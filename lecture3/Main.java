661f238b0f625f69d019a2236ddc42bac0ccd00d
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-27 22:28:55 +0530

    checking how much byte the intigeer will take

--- lecture3/Main.java
+++ lecture3/Main.java
@@ -5,8 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter some input: ");
-        int rollno = input.nextInt();
-        System.out.println("Your roll nunber is " + rollno);
-    }
+        // System.out.print("Please enter some input: ");
+        // int rollno = input.nextInt();
+        // System.out.println("Your roll nunber is " + rollno);
+
+        // adding two intigers
+
+        // int num1 = input.nextInt();
+        // int num2 = input.nextInt();
+
+        // int sum = num1 + num2;
+        // System.out.println("sum = " + sum);
+
+        // Type casting
+
+        // int num = (int)(45.24f);
+        // System.out.println(num);
+
+        // checking how much byte the intigeer will take
+
+        byte a = 40;
+        byte b = 80;
+        byte c = 100;
+        int d = (a * b) / c ;
+        System.out.println(d);
+     }
 }
