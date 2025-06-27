9ce6f213d2643ec8787b6e843d906915dcd85041
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-27 21:38:44 +0530

    input string

--- lecture3/Main.java
+++ lecture3/Main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+        System.out.println(input.next());
     }
 }
