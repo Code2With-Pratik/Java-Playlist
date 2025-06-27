f0cc63a4ae163b10f10ea34468d2345919f5b17f
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-27 21:55:28 +0530

    using the input we can print the output

--- lecture3/Main.java
+++ lecture3/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Please enter some input: ");
         int rollno = input.nextInt();
         System.out.println("Your roll nunber is " + rollno);
     }
