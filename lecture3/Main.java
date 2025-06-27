96ee9a8ed31476e88c85fa4c28e59c7a9c1a44d9
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-27 21:24:58 +0530

    println prints the output in a new line

--- lecture3/Main.java
+++ lecture3/Main.java
@@ -2,7 +2,7 @@ package lecture3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Folks ");
+        System.out.print("Hello Folks ");
         System.out.println("Hey I a am Pratik");
     }
 }
