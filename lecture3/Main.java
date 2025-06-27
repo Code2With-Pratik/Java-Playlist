8016e7360e61f6bf852b0812ce48cd6830e04f9e
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-27 21:23:47 +0530

    A new line created

--- lecture3/Main.java
+++ lecture3/Main.java
@@ -3,5 +3,6 @@ package lecture3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Folks ");
+        System.out.println("Hey I a am Pratik");
     }
 }
