cf07debdfbb39074937cdd019ad767404e57a247
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-28 15:36:59 +0530

    if else conditional statements

---
+++ lecture4/Salary.java
@@ -0,0 +1,25 @@
+package lecture4;
+
+public class Salary {
+    public static void main(String[] args) {
+        /* 
+          Syntex of if sattements:  
+             if ( boolean expression T or F ){
+                // body
+             } else {
+               // do this  
+             }
+         */
+        
+         int Salary = 24000;
+
+         if (Salary > 10000){
+            Salary = Salary + 2000;
+         }
+         else {
+            Salary = Salary + 1000;
+         }
+
+        System.out.println(Salary);
+    }
+}
