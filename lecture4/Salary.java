09bce94f6e7af696cf03e57fff943cce785678bd
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-28 15:50:24 +0530

    if else if conditions

--- lecture4/Salary.java
+++ lecture4/Salary.java
@@ -13,12 +13,24 @@ public class Salary {
         
          int Salary = 24000;
 
-         if (Salary > 10000) {
-            Salary = Salary + 2000;
-         }
-         else {
-            Salary = Salary + 1000;
-         }
+        //  if (Salary > 10000) {
+        //     Salary = Salary + 2000;
+        //  }
+        //  else {
+        //     Salary = Salary + 1000;
+        //  }
+
+        // multiple if-else statements
+
+        if (Salary > 10000) {
+            Salary += 1000;  // this statement is same as "Salary = Salary + 2000;"
+        } 
+        else if (Salary > 20000) {
+            Salary += 5000;
+        }
+        else {
+            Salary += 2000;
+        }
 
         System.out.println(Salary);
     }
