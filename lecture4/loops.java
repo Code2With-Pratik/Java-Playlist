757f9bf4db617316007358535e00dff54aab64cc
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-28 16:46:33 +0530

    while loop

--- lecture4/loops.java
+++ lecture4/loops.java
@@ -1,5 +1,6 @@
 package lecture4;
 
+import java.util.Scanner;
 
 // if we have to print same output for multiple time then we use a loop condition
 
@@ -9,7 +10,7 @@ public class loops {
 
         // System.out.println(1);
         // System.out.println(2);
-        // System.out.println(3);   This is ❌ wrong way of printing 
+        // System.out.println(3);   // This is ❌ wrong way of printing 
         // System.out.println(4);
         // System.out.println(5);
 
@@ -23,8 +24,34 @@ public class loops {
         */
 
         // Q: print Number from 1 to 5 ? 
-        for (int num = 1; num <= 5; num ++ ) {  // or you can also write " num += 1 " as " num ++ "
-            System.out.println(num);            // if i put num = 6 then the condition will be false and num will not increment hence it will throw the num out of the function 
+        // for (int num = 1; num <= 5; num ++ ) {  // or you can also write " num += 1 " as " num ++ "
+        //                                         // if i put num = 6 then the condition will be false and num will not 
+        //                                         //    increment hence it will throw the num out of the function 
+        //     System.out.println(num); 
+        // }
+
+        // Q: print number from 1 to n ?
+        // Scanner in = new Scanner(System.in);
+        // int n = in.nextInt();
+
+        // for (int num = 1; num <= n; num ++) {
+        //     // System.out.print(num + " ");
+        //     System.out.println(" Hello World ");
+        // }
+
+        // Syntex of " while Loop "  
+        /*
+          while (condition) {
+             // body 
+          }
+        */
+
+        int num = 1 ;
+        while ( num <=8 ) {
+            System.out.println(num);
+            num ++;
         }
+        
+
     }
 }
