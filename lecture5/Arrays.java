40a20716887cb0860c7e1914fb6550e30261f1e4
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-29 17:08:54 +0530

    refactor Arrays.java to enhance user input handling and array initialization

--- lecture5/Arrays.java
+++ lecture5/Arrays.java
@@ -1,5 +1,7 @@
 package lecture5;
 
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
         // // Q: Store a roll number 
@@ -24,13 +26,29 @@ public class Arrays {
         // int[] ros; // declaration of array . ros is getting defined in the stack
         // ros = new int[5]; //actually here object is being created in the memery (heap)  
 
-        int [] arr = new int[5];
-        arr[0] = 10; // first element
-        arr[1] = 20; // second element
-        arr[2] = 30; // third element
-        arr[3] = 40; // fifth element
-        arr[4] = 50; // fourth element
+        Scanner in = new Scanner(System.in);
+        
+        int [] arr = new int[10];
+        // arr[0] = 10; // first element
+        // arr[1] = 20; // second element
+        // arr[2] = 30; // third element
+        // arr[3] = 40; // fifth element
+        // arr[4] = 50; // fourth element
 
-        System.out.println(arr[4]); // prints 10
+        // what if i have to make an n number of an array so we basically use a loop
+
+        // the above arry is storing the elements as [10, 20, 30, 40, 50]
+
+        // System.out.println(arr[4]); // prints 10
+
+        // input using for loops
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt(); // taking input from the user
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " "); // taking input from the user
+        }
     }
 }
