761f95eeac6f78816c05d8c3410dba2756c337e4
Code2With-Pratik <work.pratik1204@gmail.com>
2025-06-29 16:42:04 +0530

    add Arrays.java with array initialization and example usage

---
+++ lecture5/Arrays.java
@@ -0,0 +1,36 @@
+package lecture5;
+
+public class Arrays {
+    public static void main(String[] args) {
+        // // Q: Store a roll number 
+        // int a = 101;
+
+        // // Q: Store a person's name
+        // String name = "Pratik Dhandare";
+
+        // // Q: Store a list of roll numbers
+        // int[] rnos = {101, 102, 103, 104, 105};
+
+        // // syntex of array 
+        // // dataType[] variable_Name = new dataType[size];
+
+        // // store 5 roll numbers
+        // int[] rnos = new int[5];
+
+        // // ro directly you may write it as 
+
+        // int[] rnos2 = { 21 ,23 , 24, 25, 26 };
+
+        // int[] ros; // declaration of array . ros is getting defined in the stack
+        // ros = new int[5]; //actually here object is being created in the memery (heap)  
+
+        int [] arr = new int[5];
+        arr[0] = 10; // first element
+        arr[1] = 20; // second element
+        arr[2] = 30; // third element
+        arr[3] = 40; // fifth element
+        arr[4] = 50; // fourth element
+
+        System.out.println(arr[4]); // prints 10
+    }
+}
