ec4ef1a1e3873f53cc7edc9bdfabc76c3bf2f9f4
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-02 00:15:29 +0530

    add MultiDimensional.java to demonstrate two-dimensional array declaration and initialization

---
+++ MultiDimensional.java
@@ -0,0 +1,17 @@
+public class MultiDimensional {
+    public static void main(String[] args) {
+        /*
+         *  1 2 3 
+         *  4 5 6
+         *  7 8 9
+        */
+        int[][] arr = int[3][]; // two dimensional array declaration where where the first dimension is fixed  for no. of rows
+                                            // and the second dimension is variable for no. of columns
+
+        int[][] arr = {
+            {1 , 2 , 3},
+            {4 , 5 , 6},
+            {7 , 8 , 9}
+        };
+    }
+}
