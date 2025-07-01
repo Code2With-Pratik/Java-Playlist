178211c9038c904c5b45161220dcac4951016b34
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-01 22:40:46 +0530

    enhance inputs.java to include user input handling for dynamic array initialization

--- lecture5/inputs.java
+++ lecture5/inputs.java
@@ -1,13 +1,27 @@
 package lecture5;
 
+import java.util.Scanner;
+
 public class inputs {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         int[] arr = new int[5];
         arr[0] = 10; // first element
         arr[1] = 20; // second element  
         arr[2] = 30; // third element
         arr[3] = 40; // fourth element  
         arr[4] = 50; // fifth element
+
+        // the above array is storing the elements as [10, 20, 30, 40, 50]
+        // what if I have to make an n number of an array so we basically use a loop.
         System.out.println(arr[2]); // prints 30
+
+        // input ising for loops
+        for (int i = 0; i < arr.length; i++) { // i f we don't know the size of the array we use "arr.length"
+            arr[i] = in.nextInt(); // taking input from the user
+        }
+        for (int i = 0; i < arr.length; i++) { // i f we don't know the size of the array we use "arr.length"
+              System.out.print(arr[i] + " "); // prints all elements in the array
+        }
     }
 }
