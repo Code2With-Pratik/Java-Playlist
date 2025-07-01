eb7635fb60995e9ff2939812411d3ce22ff19a4e
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-02 00:34:51 +0530

    refactor MultiDimensional.java to implement user input for a two-dimensional array and improve code clarity

--- MultiDimensional.java
+++ MultiDimensional.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MultiDimensional {
     public static void main(String[] args) {
         /*
@@ -5,13 +7,30 @@ public class MultiDimensional {
          *  4 5 6
          *  7 8 9
         */
-        int[][] arr = int[3][]; // two dimensional array declaration where where the first dimension is fixed  for no. of rows
-                                            // and the second dimension is variable for no. of columns
+        // Use try-with-resources to ensure Scanner is closed properly
+        try (Scanner in = new Scanner(System.in)) {
+            // int[][] arr = int[3][]; // two dimensional array declaration where where the first dimension is fixed  for no. of rows
+                                                // and the second dimension is variable for no. of columns
 
-        int[][] arr = {
-            {1 , 2 , 3},
-            {4 , 5 , 6},
-            {7 , 8 , 9}
-        };
+            // int[][] arr = {
+            //     {1 , 2 , 3}, // 0th index row
+            //     {4 , 5 },    // 1st index row with variable columns
+            //     {6 , 7 , 8 , 9} // 2nd index row with variable columns  arr[2] it will return {6, 7, 8, 9} and arr[2][0] it will return 6
+            // };
+
+            // input for two-dimensional array
+
+            int[][] arr = new int[3][3];
+            System.out.println(arr.length); // no. of rows 
+
+            for (int row = 0; row < arr.length; row++) {
+                // for each col in every row 
+                for (int col = 0; col < arr[row].length; col++) {
+                    System.out.print("Enter value for row " + row + " and column " + col + ": ");
+                    arr[row][col] = in.nextInt(); // input for each element in the two-dimensional array
+                }
+            }
+        }
     }
 }
+                    //    video time line = 56:03  //
