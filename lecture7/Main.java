f9ae86d8b7ebde19862194787b403b13f50dc5e6
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-04 22:11:48 +0530

    add Main.java with initial implementation for sum calculation

---
+++ lecture7/Main.java
@@ -0,0 +1,10 @@
+package lecture7;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        int sum = (a * b)/(a - b); 
+        System.out.println(sum);
+    }
+}
