7bc63ee9ce73d4dd9bbca7b6b917ebac7048ca1c
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-05 00:35:27 +0530

    Implement age eligibility check for voting and add button selection using switch case

--- lecture7/Conditionals.java
+++ lecture7/Conditionals.java
@@ -1,19 +1,19 @@
-// package lecture7;
+package lecture7;
 
-// import java.util.*;
+import java.util.*;
 
-// public class Conditionals {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.print("Enter your age:");
-//         int age = scanner.nextInt();
-//         if (age < 18) {
-//             System.out.println("You are not eligible to vote.");
-//         } else {
-//             System.out.println("You are eligible to vote.");
-//         }
-//     }
-// }
+public class Conditionals {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age:");
+        int age = scanner.nextInt();
+        if (age < 18) {
+            System.out.println("You are not eligible to vote.");
+        } else {
+            System.out.println("You are eligible to vote.");
+        }
+    }
+}
 
 // check the input number is even or odd
 
@@ -34,25 +34,25 @@
 //     }
 // }
 
-package lecture7;
+// package lecture7;
 
-import java.util.*;
+// import java.util.*;
 
-public class Conditionals {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the value of a:");
-        int a = scanner.nextInt();
-        System.out.print("Enter the value of b:");
-        int b = scanner.nextInt();
-        if (a == b) {
-            System.out.println("a is equal to b.");
-        } else {
-            if (a > b) {
-                System.out.println("a is greater than b.");
-            } else {
-                System.out.println("a is less than b.");
-            }
-        }
-    }
-}
+// public class Conditionals {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter the value of a:");
+//         int a = scanner.nextInt();
+//         System.out.print("Enter the value of b:");
+//         int b = scanner.nextInt();
+//         if (a == b) {
+//             System.out.println("a is equal to b.");
+//         } else {
+//             if (a > b) {
+//                 System.out.println("a is greater than b.");
+//             } else {
+//                 System.out.println("a is less than b.");
+//             }
+//         }
+//     }
+// }
