47c3c2295f1677eb13ba681937c6ecae75982a6d
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-04 22:18:52 +0530

    refactor Main.java to improve structure and maintain commented-out code for future reference

--- lecture7/Main.java
+++ lecture7/Main.java
@@ -1,10 +1,16 @@
 package lecture7;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 15;
-        int sum = (a * b)/(a - b); 
-        System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        System.out.println(name);
+        // int a = 20;
+        // int b = 15;
+        // int sum = (a * b)/(a - b); 
+        // System.out.println(sum);
     }
 }
