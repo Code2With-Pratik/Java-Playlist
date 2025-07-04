fb6761b22460d45d06a77f67c0f20fbc3d164579
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-04 23:53:44 +0530

    check two number is = , > , <

--- lecture7/Conditionals.java
+++ lecture7/Conditionals.java
@@ -1,19 +1,19 @@
-package lecture7;
+// package lecture7;
 
-import java.util.*;
+// import java.util.*;
 
-public class Conditionals {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your age:");
-        int age = scanner.nextInt();
-        if (age < 18) {
-            System.out.println("You are not eligible to vote.");
-        } else {
-            System.out.println("You are eligible to vote.");
-        }
-    }
-}
+// public class Conditionals {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter your age:");
+//         int age = scanner.nextInt();
+//         if (age < 18) {
+//             System.out.println("You are not eligible to vote.");
+//         } else {
+//             System.out.println("You are eligible to vote.");
+//         }
+//     }
+// }
 
 // check the input number is even or odd
 
@@ -26,7 +26,7 @@ public class Conditionals {
 //         Scanner scanner = new Scanner(System.in);
 //         System.out.print("Enter Number:");
 //         int number = scanner.nextInt();
-//         if (number % 2 == 0) {
+//         if (number % 2 == 0) {      // use "%" modulor operator to check the reminder
 //             System.out.println("It's a Even Number. ");
 //         } else {
 //             System.out.println("It's a Odd Number. ");
@@ -34,4 +34,25 @@ public class Conditionals {
 //     }
 // }
 
+package lecture7;
 
+import java.util.*;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of a:");
+        int a = scanner.nextInt();
+        System.out.print("Enter the value of b:");
+        int b = scanner.nextInt();
+        if (a == b) {
+            System.out.println("a is equal to b.");
+        } else {
+            if (a > b) {
+                System.out.println("a is greater than b.");
+            } else {
+                System.out.println("a is less than b.");
+            }
+        }
+    }
+}
