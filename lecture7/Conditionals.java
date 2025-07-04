db7920b3c0ad5ebf886120bea2e116ddcfc02e96
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-04 23:41:28 +0530

    check the input number is even or odd

--- lecture7/Conditionals.java
+++ lecture7/Conditionals.java
@@ -14,3 +14,24 @@ public class Conditionals {
         }
     }
 }
+
+// check the input number is even or odd
+
+// package lecture7;
+
+// import java.util.*;
+
+// public class Conditionals {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter Number:");
+//         int number = scanner.nextInt();
+//         if (number % 2 == 0) {
+//             System.out.println("It's a Even Number. ");
+//         } else {
+//             System.out.println("It's a Odd Number. ");
+//         }
+//     }
+// }
+
+
