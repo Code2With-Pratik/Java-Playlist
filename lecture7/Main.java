b16fddb1df8d29d1945f546f11b4d18f9bc99866
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-04 22:35:10 +0530

    refactor Main.java to enhance input handling and clarify Scanner class usage

--- lecture7/Main.java
+++ lecture7/Main.java
@@ -4,10 +4,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // take two numbers a & b as input and print their sum
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a  + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+        // String name = sc.next();   // here if you write next() then it will take only one word as input,
+                                      // or if you write nextLine() then it will take all the word
 
-        System.out.println(name);
+        // all the function comes inside the Scanner class
+        //  - next() - takes only one word as input
+        //  - nextLine() - takes all the words as input
+        //  - nextInt() - takes only integer as input
+        //  - nextDouble() - takes only double as input 62-bit 8 bytes 
+        //  - nextLong() - takes only long as input    
+        //  - nextFloat() - takes only float as input   32-bit 4 bytes
+
+        // System.out.println(name);
         // int a = 20;
         // int b = 15;
         // int sum = (a * b)/(a - b); 
