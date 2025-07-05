45a69deb6746ddab9416297516663bb2d0509d69
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-05 23:18:15 +0530

    print 4*5 hollow rectangle

--- lecture7/Pattern.java
+++ lecture7/Pattern.java
@@ -6,11 +6,32 @@ public class Pattern {
     public static void main(String[] args) {
       int m = 4;
       int n = 5;
-     for (int i = 1; i <= m; i++) {               // nested loop
-        for (int j = 1; j <=n; j++) {
-            System.out.print("* ");
-        }
-        System.out.println();
-     }
+
+   // print 4*5 Solid rectangle   
+
+   //   for (int i = 1; i <= m; i++) {    //rows           // nested loop
+   //      for (int j = 1; j <=n; j++) {    //columns
+   //          System.out.print("* ");
+   //      }
+   //      System.out.println();
+   //   }
+
+    // print 4*5 hollow rectangle
+
+   // outer loop for rows
+    for (int i = 1; i <= m; i++){
+      // inner loop for columns
+      for (int j = 1; j <= n; j++){
+            //cell (i,j)
+            // condition for printing the star
+            if ( i == 1 || j == 1 || i == m || j == n) {
+               System.out.print("* ");
+            } else {
+               System.out.print("  ");
+            }
+         }
+         System.out.println();
+    }
+
   }
 }
