263544531ec76c225b63bd2d5c891cbc36fe7759
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-08 00:47:21 +0530

    Print a Solid Rhombus

--- lecture7/Pattern.java
+++ lecture7/Pattern.java
@@ -132,5 +132,16 @@ public class Pattern {
             System.out.println();
         }
      System.out.println();
+
+     // Print a Solid Rhombus
+     for ( int i = 1; i <= 5; i++) {
+        for (int j = 1; j <=(n-i); j++) {
+            System.out.print("  ");
+        }
+        for ( int j = 1; j <= 5; j++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+     } 
   }
 }
