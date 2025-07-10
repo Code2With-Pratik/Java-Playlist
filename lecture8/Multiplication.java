a3ca58e5f0ca4a76ecbacaa0b4bfa59b387c8143
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-10 22:50:31 +0530

    Add Multiplication class with calculateProduct method and user input for product calculation

---
+++ lecture8/Multiplication.java
@@ -0,0 +1,23 @@
+package lecture8;
+
+import java.util.*;
+
+public class Multiplication {
+    public static int calculateProduct(int a , int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("product of two numbers: ");
+        // Taking inputs from user
+        System.out.print("Enter the value of a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the value of b: ");
+        int b = sc.nextInt();
+
+        System.out.println("Product of a and b is: " + calculateProduct(a, b));   // we can directly call the function
+
+    }
+}
