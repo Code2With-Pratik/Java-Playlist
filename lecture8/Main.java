4d9f059078d398b930d64abbecb88d09eb028cc4
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-10 22:24:08 +0530

    Refactor main method to improve user input prompts for sum calculation

--- lecture8/Main.java
+++ lecture8/Main.java
@@ -9,7 +9,11 @@ public class Main {
    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Sum of two integers: ");
+        // Taking input from user
+        System.out.print("Enter the value of a: ");
         int a = sc.nextInt();
+        System.out.print("Enter the value of b: ");
         int b = sc.nextInt();
 
         int sum = calculateSum(a, b); // calling the function
