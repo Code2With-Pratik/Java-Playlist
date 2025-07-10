b1b8a8243029fa62aaa7a903092daace2ffedd19
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-10 23:18:40 +0530

    Add Factorial class with method to calculate and print factorial of a given number

---
+++ lecture8/Factorial.java
@@ -0,0 +1,30 @@
+package lecture8;
+
+import java.util.Scanner;
+
+// Find the factorial of given input
+
+public class Factorial {
+    public static void printFactorial(int n) {
+        //loops
+        if (n < 0) {
+            System.out.println("Invalid Number");
+            return;
+        }
+
+        int factorial = 1;
+
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Find the factorial: ");
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        printFactorial(n);
+    }
+}
