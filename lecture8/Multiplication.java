693cdd46fd91e1dbfdef22419ae8bf7b075511dd
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-11 22:05:44 +0530

    Remove unnecessary blank line in Multiplication class

--- lecture8/Multiplication.java
+++ lecture8/Multiplication.java
@@ -8,7 +8,6 @@ public class Multiplication {
     public static int calculateProduct(int a , int b) {
         return a * b;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("product of two numbers: ");
