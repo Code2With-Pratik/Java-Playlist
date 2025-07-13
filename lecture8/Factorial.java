95df28f9eca48067d84b7720690b6356273f09b8
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-13 21:51:20 +0530

    maintain the code

--- lecture8/Factorial.java
+++ lecture8/Factorial.java
@@ -11,6 +11,7 @@ public class Factorial {
             System.out.println("Invalid Number");
             return;
         }
+        
         int factorial = 1;
 
         for (int i = n; i >= 1; i--) {
