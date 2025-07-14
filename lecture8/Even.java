68a14444d994603d2d6c3f512fe8eb7943e1ffdd
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-14 20:16:13 +0530

    to check the number is even or not

--- lecture8/Even.java
+++ lecture8/Even.java
@@ -1,7 +1,16 @@
 package lecture8;
 
+import java.util.*;
+
 public class Even {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+           if (num % 2 == 0)
+        }
         
     }
 }
