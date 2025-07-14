c885efe3f25ed782a82b2142dfebc10c2b49097a
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-14 20:32:41 +0530

    Refactor Even class to improve readability and maintainability

--- lecture8/Even.java
+++ lecture8/Even.java
@@ -1,16 +1,22 @@
 package lecture8;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Even {
+    
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Number: ");
-        int num = sc.nextInt();
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-           if (num % 2 == 0)
+        if (isEven(number)) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
         }
-        
     }
 }
