fddd23b3f15e50c946df5c73f73f7030753e403e
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-14 20:47:16 +0530

    Refactor variable name for Scanner in Table class for consistency

--- lecture8/Table.java
+++ lecture8/Table.java
@@ -10,11 +10,11 @@ public class Table {
     }
 
      public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // Take input from user 
         System.out.print("Enter a number to print its table: ");
-        int number = scanner.nextInt();
+        int number = sc.nextInt();
 
         // Print the multiplication table for the given number
         System.out.println("Multiplication Table for " + number + ":");
