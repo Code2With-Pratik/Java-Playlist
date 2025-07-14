4233176200c8fc9310ca46027e6c8e525a1f58e6
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-14 21:47:34 +0530

    Add OddSum class to calculate the sum of all odd numbers from 1 to n

---
+++ lecture8/OddSum.java
@@ -0,0 +1,27 @@
+package lecture8;
+
+import java.util.*;
+
+public class OddSum {
+
+    // Function to calculate sum of all odd numbers from 1 to n
+    public static int sumOfOddNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i += 2) {  // i+=2 ensures we only loop through odd numbers
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        // Call the function and print result
+        int result = sumOfOddNumbers(n);
+        System.out.println("Sum of all odd numbers from 1 to " + n + " is: " + result);
+    }
+}
