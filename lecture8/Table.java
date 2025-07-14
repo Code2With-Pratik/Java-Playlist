c0fb2d21f0bd15f1b02d54539dae608f2f2b0ec2
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-14 20:46:36 +0530

    Add Table class to print multiplication tables for user input

---
+++ lecture8/Table.java
@@ -0,0 +1,23 @@
+package lecture8;
+
+import java.util.*;
+
+public class Table {
+    public static void printTable (int number) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input from user 
+        System.out.print("Enter a number to print its table: ");
+        int number = scanner.nextInt();
+
+        // Print the multiplication table for the given number
+        System.out.println("Multiplication Table for " + number + ":");
+        printTable(number);
+   }
+}
