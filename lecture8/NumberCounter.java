68ef7582f5207a87c6df6ceb7e36d9e207abfd27
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-14 22:15:34 +0530

    display the count of positive, negative and zeros entered

---
+++ lecture8/NumberCounter.java
@@ -0,0 +1,40 @@
+package lecture8;
+
+import java.util.Scanner;
+
+public class NumberCounter {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+
+        char choice;
+
+        do {
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+
+            // Count based on number value
+            if (num > 0) {
+                positiveCount++;
+            } else if (num < 0) {
+                negativeCount++;
+            } else {
+                zeroCount++;
+            }
+
+            System.out.print("Do you want to enter another number? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+        // Display result
+        System.out.println("\nSummary:");
+        System.out.println("Positive numbers: " + positiveCount);
+        System.out.println("Negative numbers: " + negativeCount);
+        System.out.println("Zeros: " + zeroCount);
+    }
+}
