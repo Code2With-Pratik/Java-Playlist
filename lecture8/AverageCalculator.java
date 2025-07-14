1b62864b39c4af2f70b56c8bc1985f559865b557
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-14 21:03:35 +0530

    Add AverageCalculator class to compute the average of three numbers

---
+++ lecture8/AverageCalculator.java
@@ -0,0 +1,30 @@
+package lecture8;
+
+import java.util.Scanner;
+
+public class AverageCalculator {
+
+    // Function to calculate average of 3 numbers
+    public static double average(int a, int b, int c) {
+        return (a + b + c) / 3.0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking 3 numbers from the user
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        // Calculating and displaying average
+        double result = average(num1, num2, num3);
+        System.out.println("Average of the 3 numbers is: " + result);
+    }
+}
+
