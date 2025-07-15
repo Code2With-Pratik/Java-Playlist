2ad10e783ffc759568bebca126ba244326f56b7e
Code2With-Pratik <work.pratik1204@gmail.com>
2025-07-15 20:22:20 +0530

    count +ve , -ve and 0

--- lecture8/NumberCounter.java
+++ lecture8/NumberCounter.java
@@ -1,3 +1,6 @@
+// Write a program to enter the numbers till the user wants and at the end 
+// it should display the count of positive, negative and zeros entered.
+
 package lecture8;
 
 import java.util.Scanner;
