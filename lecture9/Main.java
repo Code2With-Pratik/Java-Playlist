ac7246bbe18a722a01fef5c1a050c90aa235724d
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-19 21:22:13 +0530

    large array

--- lecture9/Main.java
+++ lecture9/Main.java
@@ -6,8 +6,11 @@ public class Main {
         marks[0] = 93;  // physics
         marks[1] = 96;   // chemistry
         marks[2] = 98;   // Mathematics
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);       // with the help of this we can print the multiple and large array
+        }
     }
 }
