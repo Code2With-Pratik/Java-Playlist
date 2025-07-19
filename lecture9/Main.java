d081f2f0f47f13eb5fb8d6548dbe3f727b16b3d3
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-19 21:23:30 +0530

    you can write [] in both datatype and arrayName

--- lecture9/Main.java
+++ lecture9/Main.java
@@ -2,7 +2,7 @@ package lecture9;
 
 public class Main {
     public static void main(String[] args) {
-        int[] marks = new int[3];      // type[] arrayName = new type[size];
+        int marks[] = new int[3];      // type[] arrayName = new type[size];
         marks[0] = 93;  // physics
         marks[1] = 96;   // chemistry
         marks[2] = 98;   // Mathematics
