55729b09637e7eda38af9bb01a641db4834fd4bf
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-19 21:17:39 +0530

    Printing a garbage value

--- lecture9/Main.java
+++ lecture9/Main.java
@@ -2,6 +2,10 @@ package lecture9;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[] marks = new int[3];      // type[] arrayName = new type[size];
+        marks[0] = 93;  // physics
+        marks[1] = 96;   // chemistry
+        marks[2] = 98;   // Mathematics
+        System.out.println(marks);
     }
 }
