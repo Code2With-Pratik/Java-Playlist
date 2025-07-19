7fcd5a7f8e463c62319d8339d85f06eace50ec8e
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-19 21:30:23 +0530

    we can also write the array like this if we know the no. of elements or the size

--- lecture9/Main.java
+++ lecture9/Main.java
@@ -2,13 +2,14 @@ package lecture9;
 
 public class Main {
     public static void main(String[] args) {
-        int marks[] = new int[3];      // type[] arrayName = new type[size];
-        marks[0] = 93;  // physics
-        marks[1] = 96;   // chemistry
-        marks[2] = 98;   // Mathematics
+        // int marks[] = new int[3];      // type[] arrayName = new type[size];
+        // marks[0] = 93;  // physics
+        // marks[1] = 96;   // chemistry
+        // marks[2] = 98;   // Mathematics
         // System.out.println(marks[0]);
         // System.out.println(marks[1]);
         // System.out.println(marks[2]);
+        int marks[] = {98, 95, 93};   // we can also write the array like this if we know the no. of elements or the size
         for (int i = 0; i < 3; i++) {
             System.out.println(marks[i]);       // with the help of this we can print the multiple and large array
         }
