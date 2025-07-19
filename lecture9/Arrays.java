e6fe610928e9596231d439a07538c3e9cdec971b
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-19 21:55:53 +0530

    creating an array from user input

---
+++ lecture9/Arrays.java
@@ -0,0 +1,15 @@
+package lecture9;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+
+        for ( int i=0; i<size; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
