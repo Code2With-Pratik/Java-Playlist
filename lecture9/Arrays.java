e78f0fb726bf38a80d762796fe0b358c8df6e8eb
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-19 22:13:43 +0530

    find the index of elements

--- lecture9/Arrays.java
+++ lecture9/Arrays.java
@@ -13,9 +13,13 @@ public class Arrays {
         for (int i=0; i<size; i++) {
             numbers[i] = sc.nextInt();
         }
+
+        int x =sc.nextInt();
         //output
-        for ( int i=0; i<size; i++) {
-            System.out.println(numbers[i]);
+        for (int i=0; i<numbers.length; i++) {
+            if (numbers[i] == x) {
+                System.out.println("x found at index : " + i);
+            }
         }
     }
 }
