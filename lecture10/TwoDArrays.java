74da4e1533c6ad3f24f6877262cd0d7f49850430
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-21 13:58:54 +0530

    Taking a matrix as an input and printing its elements

--- lecture10/TwoDArrays.java
+++ lecture10/TwoDArrays.java
@@ -2,34 +2,33 @@ package lecture10;
 
 import java.util.*;
 
-
 public class TwoDArrays {
-   public static void main(String args[]) {
-       Scanner sc = new Scanner(System.in);
-       int rows = sc.nextInt();
-       int cols = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows:");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns:");
+        int cols = sc.nextInt();
 
+        int[][] arr = new int[rows][cols];
 
-       int[][] numbers = new int[rows][cols];
+        System.out.print("Enter the elements of the array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
 
+        System.out.println("The entered array is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
 
-       //input
-       //rows
-       for(int i=0; i<rows; i++) {
-           //columns
-           for(int j=0; j<cols; j++) {
-               numbers[i][j] = sc.nextInt();
-           }
-       }
-
-
-
-
-       for(int i=0; i<rows; i++) {
-           for(int j=0; j<cols; j++) {
-                   System.out.print(numbers[i][j]+" ");
-               }
-               System.out.println();
-           }
-   }
+        sc.close();
+        
+    }
+    
 }
