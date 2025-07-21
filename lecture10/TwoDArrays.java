58e78b4f37285b9a211fc9d9f4e8cf0d7338ed90
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-21 14:14:01 +0530

    added useful comments

--- lecture10/TwoDArrays.java
+++ lecture10/TwoDArrays.java
@@ -4,14 +4,17 @@ import java.util.*;
 
 public class TwoDArrays {
     public static void main(String[] args) {
+        //✅ Take Number of Rows and Columns
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows:");
         int rows = sc.nextInt();
         System.out.print("Enter the number of columns:");
         int cols = sc.nextInt();
 
+        //✅ Create 2D Array
         int[][] arr = new int[rows][cols];
 
+        //✅ Take Input for the Array
         System.out.print("Enter the elements of the array:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -19,6 +22,7 @@ public class TwoDArrays {
             }
         }
 
+        // ✅ Print the Array
         System.out.println("The entered array is:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -27,6 +31,7 @@ public class TwoDArrays {
             System.out.println();
         }
 
+        //✅ Close Scanner
         sc.close();
         
     }
