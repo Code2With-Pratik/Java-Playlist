f5dc49aec17123f9046e603a31c12f42ae78aeef
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-22 19:43:57 +0530

    print each lettter of string using charAt() function

--- lecture12/Concatenation.java
+++ lecture12/Concatenation.java
@@ -13,6 +13,12 @@ public class Concatenation {
         // String lastName = "Dhandare";
         String fullName = firstName + " " + lastName;
         System.out.println(fullName);
+        // to find the length of a complete sting we use length keyword
+        System.out.println(fullName.length());
+        // to print eaech letter in the string we use charAt function
+        for (int i=0; i<fullName.length(); i++) {
+            System.out.println(fullName.charAt(i));
+        }
     }
     
 }
