98d13548f9fcf2aa235a59c6aca8a1dc857cf274
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-25 17:02:18 +0530

    write a java program to print the reverse of a input string

--- lecture13/ReverseString.java
+++ lecture13/ReverseString.java
@@ -5,9 +5,13 @@ import java.util.*;
 public class ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the String to be reversed: ");
-        String str = sc.nextLine();
-        StringBuilder sb = new StringBuilder(str);
-        System.out.println("Reversed String: " + sb.reverse().toString());
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(input);
+        System.out.println("Reversed string: " + sb.reverse());
+
+        sc.close();
     }
 }
