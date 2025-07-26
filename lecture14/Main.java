caf545211567c54cb7f82d217869b05b85cebb93
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-26 21:55:45 +0530

    reversing a string

--- lecture14/Main.java
+++ lecture14/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Today is holiday");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String name = sc.nextLine();
+        StringBuilder newName = new StringBuilder(name);
+        System.out.println(newName.reverse());
+    
     }
 }
