edf01f2564a03ed9611907f5471d5e9468868338
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-27 23:38:17 +0530

    take a string as an  input

--- lecture14/Main.java
+++ lecture14/Main.java
@@ -5,6 +5,7 @@ public class Main {
         // this will print the message today is holiday
         System.out.println("Today is holiday");
         Scanner sc = new Scanner(System.in);
+        // this will take a input from the user
         System.out.print("Enter the string: ");
         String name = sc.nextLine();
         StringBuilder newName = new StringBuilder(name);
