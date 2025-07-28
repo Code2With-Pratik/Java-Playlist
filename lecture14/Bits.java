73b36fcfba369b736e61cd861623d93a6022c9e3
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-28 23:13:00 +0530

    to check the bit wise integer are on or off

--- lecture14/Bits.java
+++ lecture14/Bits.java
@@ -4,6 +4,13 @@ public class Bits {
     public static void main(String[] args) {
         int n = 5; // 0101;
         int pos = 3; // position to check
+        int bitMask = 1 << (pos - 1); // Create a bitmask for the given position
+
+        if ((bitMask & n) == 0) {
+            System.out.println("The bit at position " + pos + " is OFF.");
+        } else {
+            System.out.println("The bit at position " + pos + " is ON.");
+        } 
 
     }
 }
