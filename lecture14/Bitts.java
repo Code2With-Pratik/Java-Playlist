e29c10ed1ac49c1156b04c60bc9126df589f8a17
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-29 00:23:09 +0530

    create a bitmask with 1 at position 2, which is 0000 0100 in binary

--- lecture14/Bitts.java
+++ lecture14/Bitts.java
@@ -5,10 +5,10 @@ public class Bitts {
     public static void main(String[] args) {
         int n = 5;  //0101
         int pos = 2; // clear bit at position 2
-        int bitMask = 1<<pos;
+        int bitMask = 1<<pos;  // create a bitmask with 1 at position 2, which is 0000 0100 in binary
         int newBitMask = ~(bitMask); // invert the bitmask
-        int newNumber = newBitMask & n;
-        System.out.println(newNumber);
+        int newNumber = newBitMask & n;  // clear the bit at position 2
+        System.out.println(newNumber);  // this will print 1, which is 0001 in binary
 
         // this wull return the number with the bit at position 2 cleared
         // 0101 & 1101 = 0101 
