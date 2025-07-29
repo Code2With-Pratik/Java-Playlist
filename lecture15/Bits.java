b6e37ef69196336e7ee4c56fab99e92663c2c3b9
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-29 23:15:35 +0530

    to set a bit in the given number

--- lecture15/Bits.java
+++ lecture15/Bits.java
@@ -2,13 +2,26 @@ package lecture15;
 
 import java.util.*;
 
-public class Bits {
+public class Bits {  // this is to get the bit at a specific position in an integral
     public static void main(String[] args) {
-        int n = 5;
-        int pos = 2;
-        int bitMask = 1<<pos;
+        int n = 5;  // 0101
+        int pos = 1;
+        int bitMask = 1<<pos;     // 1 is left shifted by pos which is 1
 
-        if ((bitMask & n) == 0) {
+        if ((bitMask & n) == 0) {   //AND operation
+            System.out.println("number is zero");
+        } else {
+            System.out.println("number is one");
+        }
+
+        // we set a bit to the given number
+
+        // all the things are same as above n , pos, bitMask
+        // here just change the logic in above we use AND gate here we use OR gate
+
+        int newNumber = bitMask | n;
+        System.out.println("new number is " + newNumber);
+        if ((bitMask | n) == 0) {   //OR operation
             System.out.println("number is zero");
         } else {
             System.out.println("number is one");
