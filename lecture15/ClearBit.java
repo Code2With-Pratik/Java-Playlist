7acd26859134a7b6866354a1b5ff02d0873e8171
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-29 23:40:16 +0530

    too clear the nit we uses ~ to chage 0 to 1 and vice versa

---
+++ lecture15/ClearBit.java
@@ -0,0 +1,14 @@
+package lecture15;
+
+import java.util.*;
+
+public class ClearBit {
+    public static void main(String[] args) {
+        int n = 5;   // 0101
+        int pos = 2;
+        int bitMask = 1 << pos;
+        int notBitMask = ~(bitMask);
+        int newNumber = notBitMask & n;
+        System.out.println("New Number is: " + newNumber);
+    }
+}
