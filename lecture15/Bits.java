568252e49b2623d6c0a52e71be7c2b1a8d93207b
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-29 22:59:45 +0530

    to shift a bit using left shift operator

---
+++ lecture15/Bits.java
@@ -0,0 +1,17 @@
+package lecture15;
+
+import java.util.*;
+
+public class Bits {
+    public static void main(String[] args) {
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1<<pos;
+
+        if ((bitMask & n) == 0) {
+            System.out.println("number is zero");
+        } else {
+            System.out.println("number is one");
+        }
+    }
+}
