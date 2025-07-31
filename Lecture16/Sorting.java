6e7098d3c7f106f32755d4e2b0f8cd30b50fb20a
Pratik Dhandare <work.pratik1204@gmail.com>
2025-07-31 21:15:14 +0530

    Selection sorting in java

--- Lecture16/Sorting.java
+++ Lecture16/Sorting.java
@@ -14,8 +14,8 @@ public class Sorting {
          int arr[] = {7,2,1,6,8,5};
 
         //  bubble sorting 
-        for (int i=0; i<arr.length-1; i++) {
-            for (int j=0; j<arr.length-i-1; j++) {
+        for (int i=0; i<arr.length-1; i++) {  //n-1
+            for (int j=0; j<arr.length-i-1; j++) {   //n-1-i
                 if (arr[j] > arr[j+1]) {
                     //swap
                     int temp = arr[j];
@@ -24,8 +24,21 @@ public class Sorting {
 
                 }
             }
-        }
+        }  //time complexity is O(n^2)
 
+        // Selection sorting
+        for (int i=0; i<arr.length-1; i++) {
+            int smallest = i;
+            for (int j=i+1; j<arr.length; j++) {
+                if (arr[j] < arr[smallest]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }  //time complexity is O(n^2)
+ 
         printArray(arr);
     }
 }
