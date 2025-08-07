ff08b69684eb19ef52ae269c844c937df91e8b12
Pratik Dhandare <work.pratik1204@gmail.com>
2025-08-07 22:44:44 +0530

    here a new function starts which actually traverse the whole array

--- Lecture16/Question6.java
+++ Lecture16/Question6.java
@@ -10,11 +10,9 @@ public class Question6 {
          }
          System.out.println();
      }
-    
-    
+    // here a new function starts which actually traverse the whole array
      public static void main(String args[]) {
          int arr[] = {7, 8, 1, 3, 2};
-    
     
          //insertion sort
          for(int i=1; i<arr.length; i++) {
