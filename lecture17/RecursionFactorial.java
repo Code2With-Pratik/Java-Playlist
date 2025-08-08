55229317ee6d634dee2353bfac5175709ce16cab
Pratik Dhandare <work.pratik1204@gmail.com>
2025-08-08 21:50:53 +0530

    removed extra spaces

--- lecture17/RecursionFactorial.java
+++ lecture17/RecursionFactorial.java
@@ -5,7 +5,6 @@ public class RecursionFactorial {
         if (n == 0 || n == 1) return 1; // base case
         return n * factorial(n - 1);    // recursive step
     }
-
     public static void main(String[] args) {
         int n = 5;
         System.out.println("Factorial: " + factorial(n));
