6506886e019ac3b55c3b822cdd83164307556879
Pratik Dhandare <work.pratik1204@gmail.com>
2025-08-08 21:42:56 +0530

    Reverse a string using recursion.

---
+++ lecture17/RecursionReverseString.java
@@ -0,0 +1,13 @@
+package lecture17;
+
+public class RecursionReverseString {
+    static String reverse(String str) {
+        if (str.isEmpty()) return str; // base case
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println("Reversed: " + reverse(str));
+    }
+}
