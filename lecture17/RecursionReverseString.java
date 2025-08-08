9c9df8d9462680317dadea738671bba0665f42b0
Pratik Dhandare <work.pratik1204@gmail.com>
2025-08-08 21:51:20 +0530

    code debuged

--- lecture17/RecursionReverseString.java
+++ lecture17/RecursionReverseString.java
@@ -5,7 +5,6 @@ public class RecursionReverseString {
         if (str.isEmpty()) return str; // base case
         return reverse(str.substring(1)) + str.charAt(0);
     }
-
     public static void main(String[] args) {
         String str = "hello";
         System.out.println("Reversed: " + reverse(str));
