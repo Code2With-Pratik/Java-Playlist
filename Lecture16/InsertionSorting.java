da4dde78fd30b8221b83d7c5efef4b7cf64c3688
Code2With-Pratik <work.pratik1204@gmail.com>
2025-08-12 22:25:48 +0530

    Update InsertionSorting.java

--- Lecture16/InsertionSorting.java
+++ Lecture16/InsertionSorting.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class InsertionSorting {
 
-    //creating a function called printArray
+    //creating a function called print array.
     public static void printArray(int arr[]) {
         for (int i=0; i<arr.length; i++) {
             System.out.print(" " + arr[i]);
