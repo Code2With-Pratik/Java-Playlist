06a9e0da1785b71740bdcc6c8e023b0b9a9bed85
Pratik Dhandare <work.pratik1204@gmail.com>
2025-08-16 19:46:50 +0530

    Move all 'x' to the end of the string

---
+++ lecture18/Recursion2.java
@@ -0,0 +1,28 @@
+// Q.Move all 'x' to the end of the string?
+
+package lecture18;
+
+public class Recursion2 {
+
+    public static void moveAll(String str, int idx, int count, String newString) {
+        if (idx == str.length()) {
+            for(int i=0; i<count; i++) {
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar == 'x') {
+            count ++;
+            moveAll(str, idx+1, count, newString);
+        } else {
+            newString += currChar;   // newString = newString + currChar
+            moveAll(str, idx+1, count, newString);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "axbcxxd";
+        moveAll(str, 0, 0, "");
+    }
+}
