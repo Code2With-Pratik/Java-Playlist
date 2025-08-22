53e967fbc90f3b7c1bff739f1c4a301b0e382691
Pratik Dhandare <work.pratik1204@gmail.com>
2025-08-22 19:18:58 +0530

    define an integer

--- lecture18/Recursion4.java
+++ lecture18/Recursion4.java
@@ -4,6 +4,7 @@ public class Recursion4 {
 
     public static void removeDuplicates(String str, int idx, String newString) {
         String str ="Pratik";
+        int idx = 0;
     }
     public static void main(String[] args) {
         
